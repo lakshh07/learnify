65d59d96f326621b6193e8e9a593b1483079f0df
Lakshay Maini <62194911+lakshh07@users.noreply.github.com>
2022-07-15 10:30:41 +0530

    added routes to navbar

--- packages/next-app/pages/components/Navbar.jsx
+++ packages/next-app/pages/components/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { Flex, Heading, Image, Link } from "@chakra-ui/react";
+import { Flex, Heading, Image, Link, Text } from "@chakra-ui/react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useRouter } from "next/router";
 
 function Navbar() {
+  const router = useRouter();
+
   return (
     <>
-      <Flex px={"4em"} py={"1.5em"} justifyContent={"space-between"}>
+      <Flex
+        px={"4em"}
+        py={"1.5em"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+      >
         <Link href="/" _hover={{ textDecoration: "none" }}>
           <Flex alignItems={"center"}>
             <Image
@@ -25,7 +33,43 @@ function Navbar() {
           </Flex>
         </Link>
 
-        <ConnectButton />
+        {router.asPath === "/" ? null : (
+          <Flex alignItems={"center"}>
+            <Link _hover={{ textDecoration: "none" }} href="/courses">
+              <Text
+                fontSize={"1.125rem"}
+                color={"#0a0a0a"}
+                lineHeight={"1.625rem"}
+                mr={"1em"}
+                transition="color 0.2s ease"
+                _hover={{ color: "gray", transition: "color 0.2s ease" }}
+                fontWeight={
+                  router.asPath === "/courses" ||
+                  router.asPath === "/courses/:id"
+                    ? 600
+                    : 400
+                }
+              >
+                Courses
+              </Text>
+            </Link>
+            <Link href="/quests" _hover={{ textDecoration: "none" }}>
+              <Text
+                fontSize={"1.125rem"}
+                color={"#0a0a0a"}
+                lineHeight={"1.625rem"}
+                ml={"1em"}
+                transition="color 0.2s ease"
+                _hover={{ color: "gray", transition: "color 0.2s ease" }}
+                fontWeight={router.asPath === "/quests" ? 600 : 400}
+              >
+                Quests
+              </Text>
+            </Link>
+          </Flex>
+        )}
+
+        {router.asPath === "/" ? null : <ConnectButton />}
       </Flex>
     </>
   );
